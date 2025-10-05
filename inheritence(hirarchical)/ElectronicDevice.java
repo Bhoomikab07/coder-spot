bde6ced370df9902572a8dc65b21e63fc18e3d5d
Bhoomikab07 <bhoomikab.xworkz@gmail.com>
2025-10-05 14:28:00 +0530

    inheritence(hirarchical)

---
+++ inheritence(hirarchical)/ElectronicDevice.java
@@ -0,0 +1,8 @@
+class ElectronicDevice{
+	public void displayElectronicDevice(){
+		System.out.println("its a first class");
+		System.out.println("----------------------");
+		System.out.println("this is a Device class");
+		
+	}
+}
---
+++ inheritence(hirarchical)/Laptop.java
@@ -0,0 +1,7 @@
+class Laptop extends ElectronicDevice{
+	public void displayLaptop(){
+		System.out.println("its a second class");
+		System.out.println("----------------------");
+		System.out.println("this is a Laptop class");
+	}
+}
