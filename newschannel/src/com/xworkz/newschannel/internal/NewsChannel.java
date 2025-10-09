5762c58fbeb490375fc097a5c1295b15dac10eba
Bhoomika07 <bhoomikab.xworkz>
2025-10-09 13:22:02 +0530

    newschannel

---
+++ newschannel/src/com/xworkz/newschannel/internal/NewsChannel.java
@@ -0,0 +1,10 @@
+package com.xworkz.newschannel.internal;
+
+public abstract class NewsChannel {
+    public abstract void name();
+
+    
+    public void ratings(){
+
+    }
+}
---
+++ newschannel/src/com/xworkz/newschannel/internal/SuvarnaNews.java
@@ -0,0 +1,16 @@
+package com.xworkz.newschannel.internal;
+
+public class SuvarnaNews extends NewsChannel {
+    @Override
+    public void name(){
+
+
+
+    }
+
+    @Override
+    public void ratings() {
+        super.ratings();
+
+}
+}
